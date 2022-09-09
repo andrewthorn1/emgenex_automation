25957d625cab34684bd553ec69380b3b49ff257d
LAPTOP-K7Q969NR\andre <andrewthorn1@gmail.com>
2022-09-09 17:51:27 -0500

    Add test_button_learn_more_in_long_term_care_section test

--- tests/unit/test_homepage.py
+++ tests/unit/test_homepage.py
@@ -1,5 +1,8 @@
+import time
+
 import pytest
 from pom.homepage_nav import HomepageNav
+from pom.longtermcare_nav import LongTermCareNav
 
 
 @pytest.mark.usefixtures('setup')
@@ -14,3 +17,14 @@ class TestHomepage:
     def test_first_big_right_banner_is_visible(self):
         homepage_nav = HomepageNav(self.driver)
         assert homepage_nav.get_first_big_right_banner(), 'First big right banner is not visible'
+
+    def test_button_learn_more_in_long_term_care_section(self):
+        homepage_nav = HomepageNav(self.driver)
+        longtermcare_nav = LongTermCareNav(self.driver)
+        button = homepage_nav.get_button_learn_more_in_long_term_care_section()
+        homepage_nav.driver.execute_script("window.scrollBy(0,1000);")
+        homepage_nav.hover_on_long_term_care_section()
+        button.click()
+        time.sleep(1)
+        actual_title = longtermcare_nav.get_title_text()
+        assert actual_title == "For Long Term Care"
