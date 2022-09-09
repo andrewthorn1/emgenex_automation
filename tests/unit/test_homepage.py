fe69bd45d00d4f35e0a10c80a2aa74b281590c3f
LAPTOP-K7Q969NR\andre <andrewthorn1@gmail.com>
2022-09-09 18:05:54 -0500

    Add pytest marks

--- tests/unit/test_homepage.py
+++ tests/unit/test_homepage.py
@@ -8,16 +8,20 @@ from pom.longtermcare_nav import LongTermCareNav
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
+    @pytest.mark.regression
     def test_nav_links(self):
         homepage_nav = HomepageNav(self.driver)
         actual_links = homepage_nav.get_nav_links_text()
         expected_links = homepage_nav.NAV_LINK_TEXT
         assert expected_links == actual_links, 'Validating Header Nav Links Text'
 
+    @pytest.mark.regression
     def test_first_big_right_banner_is_visible(self):
         homepage_nav = HomepageNav(self.driver)
         assert homepage_nav.get_first_big_right_banner(), 'First big right banner is not visible'
 
+    @pytest.mark.smoke
+    @pytest.mark.regression
     def test_button_learn_more_in_long_term_care_section(self):
         homepage_nav = HomepageNav(self.driver)
         longtermcare_nav = LongTermCareNav(self.driver)
