3e7f54792eb6cfb95936082f143aa486b7bbd2b3
LAPTOP-K7Q969NR\andre <andrewthorn1@gmail.com>
2022-09-10 12:53:43 -0500

    Add 2 tests for "Get Started" form

---
+++ tests/integration/test_get_started_today_form.py
@@ -0,0 +1,32 @@
+import time
+
+import pytest
+from pom.homepage_nav import HomepageNav
+from pom.longtermcare_nav import LongTermCareNav
+
+
+@pytest.mark.usefixtures('setup')
+class TestGetStartForm:
+
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_with_all_required_fields(self):
+        homepage_nav = HomepageNav(self.driver)
+        homepage_nav.driver.execute_script("window.scrollBy(0,4600);")
+        homepage_nav.get_first_name_field().send_keys("Test FirstName")
+        homepage_nav.get_last_name_field().send_keys("Test LastName")
+        homepage_nav.get_email_field().send_keys("testemail@gmail.com")
+        homepage_nav.get_submit_button().click()
+        actual_message_text = homepage_nav.get_submitted_message_text()
+        assert actual_message_text == "Thanks for submitting the form.", "Problems with Get Started form"
+        # time.sleep(100000)
+
+    @pytest.mark.regression
+    def test_with_all_required_fields(self):
+        homepage_nav = HomepageNav(self.driver)
+        homepage_nav.driver.execute_script("window.scrollBy(0,4600);")
+        homepage_nav.get_first_name_field().send_keys("Test FirstName")
+        homepage_nav.get_last_name_field().send_keys("Test LastName")
+        homepage_nav.get_submit_button().click()
+        actual_error_text = homepage_nav.get_error_message_text()
+        assert actual_error_text == "Please complete all required fields."
