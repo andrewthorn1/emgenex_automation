19320b104d9c2c0e8cfe103c1b929354c5c30b00
LAPTOP-K7Q969NR\andre <andrewthorn1@gmail.com>
2022-09-10 12:54:21 -0500

    Add navigation for testing "Get Started" form

--- pom/homepage_nav.py
+++ pom/homepage_nav.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-
 class HomepageNav(SeleniumBase):
 
     def __init__(self, driver):
@@ -20,6 +19,12 @@ class HomepageNav(SeleniumBase):
         self.__button_learn_more_in_long_term_care_section_locator: str = '//div[@class="EwThreeColInner"]//a[@href=' \
                                                                           '"https://www.emgenex.com/long-term-care"]'
         self.NAV_LINK_TEXT = 'Home,Solutions,About Us,Contact Us'
+        self.__first_name_locator: str = 'firstname'
+        self.__last_name_locator: str = 'lastname'
+        self.__email_locator: str = 'email'
+        self.__submit_button_locator: str = 'input[value="Submit"]'
+        self.__submitted_message_locator: str = '.submitted-message'
+        self.__error_message_locator: str = 'div[class="hs_error_rollup"]'
 
     def get_nav_links(self) -> List[WebElement]:
         return self.are_visible('css', self.__nav_links, 'Header Navigation Links')
@@ -43,3 +48,21 @@ class HomepageNav(SeleniumBase):
 
     def get_contact_us_link(self) -> WebElement:
         return self.is_visible('xpath', self.__contact_us_link_locator)
+
+    def get_first_name_field(self) -> WebElement:
+        return self.is_visible("name", self.__first_name_locator)
+
+    def get_last_name_field(self) -> WebElement:
+        return self.is_visible("name", self.__last_name_locator)
+
+    def get_email_field(self) -> WebElement:
+        return self.is_visible("name", self.__email_locator)
+
+    def get_submit_button(self) -> WebElement:
+        return self.is_visible("css", self.__submit_button_locator)
+
+    def get_submitted_message_text(self) -> str:
+        return self.get_text_from_webelement("css", self.__submitted_message_locator)
+
+    def get_error_message_text(self) -> str:
+        return self.get_text_from_webelement("css", self.__error_message_locator)
